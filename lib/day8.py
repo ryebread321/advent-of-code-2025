c9112f7d867e9abc6b821d5bdda6fe5cfb01bf9e
Ryan <github893@proton.me>
2025-12-25 16:05:51 -0500

    feat: add working solution for day 8 - part 1

--- lib/day8.py
+++ lib/day8.py
@@ -1,4 +1,4 @@
-import collections
+import copy
 import dataclasses
 import heapq
 import itertools
@@ -7,9 +7,6 @@ from typing import Iterable
 import networkx as nx
 import numpy as np
 from numpy.typing import NDArray
-
-# Use named tuples for convience when computing distances.
-Position = collections.namedtuple("Position", "x y z")
 
 
 # Use left value as the tie breaker when distances are equal
@@ -25,56 +22,55 @@ class HeapItem:
 Heap = list[HeapItem]
 
 
-def parse_position(position: str) -> Position:
+def parse_position(position: str) -> NDArray:
     x, y, z = position.split(",")
-    return Position(x=int(x), y=int(y), z=int(z))
+    return np.array([int(x), int(y), int(z)])
 
 
-def parse_positions(positions: Iterable[str]) -> list[Position]:
+def parse_positions(positions: Iterable[str]) -> list[NDArray]:
     return [parse_position(p) for p in positions]
 
 
-def build_distance_matrix(positions: list[Position]) -> NDArray:
-    distances = np.zeros((len(positions), len(positions)))
-    for i, pi in enumerate(positions):
-        for j, pj in enumerate(positions):
-            distances[i, j] = np.linalg.norm(np.array(pi) - np.array(pj))
+def build_distance_matrix(positions: list[NDArray]) -> dict[tuple, float]:
+    distances = {}
+    for i, p_i in enumerate(positions):
+        for j, p_j in enumerate(positions):
+            if i < j:
+                distances[(i, j)] = np.linalg.norm(p_i - p_j)
     return distances
 
 
-def build_heap(distances: NDArray) -> Heap:
+def build_heap(distances: dict[tuple, float]) -> Heap:
     heap = []
-    indices = range(len(distances))
-    for i, j in itertools.product(indices, repeat=2):
-        if i != j:
-            item = HeapItem(distance=distances[i, j], left=i, right=j)
-            heapq.heappush(heap, item)
+    for (i, j), distance in distances.items():
+        item = HeapItem(distance=distance, left=i, right=j)
+        heapq.heappush(heap, item)
     return heap
 
 
 def compute_components(heap: Heap, n: int) -> Iterable[set]:
-    heap = heap.copy()
+    heap = copy.copy(heap)
     graph = nx.Graph()
-    for _ in range(n):
+    for _ in range(min(len(heap), n)):
         item = heapq.heappop(heap)
         graph.add_edge(item.left, item.right)
     return nx.algorithms.components.connected_components(graph)
 
 
-def compute_component_size_product(components: Iterable[set], n: int) -> int:
+def compute_component_size_product(components: Iterable[set], k: int) -> int:
     sizes = sorted(map(len, components), reverse=True)
-    return int(np.prod(sizes[:n]))
+    return int(np.prod(sizes[:k]))
 
 
-def solve_part_1() -> int:
-    with open("data/day8.txt") as f:
+def solve_part_1(filepath: str, n: int, k: int) -> int:
+    with open(filepath) as f:
         positions = parse_positions(f)
         distances = build_distance_matrix(positions)
         heap = build_heap(distances)
-        components = compute_components(heap, 1000)
-        product = compute_component_size_product(components, 3)
+        components = compute_components(heap, n)
+        product = compute_component_size_product(components, k)
         return product
 
 
 if __name__ == "__main__":
-    print(f"Part 1: {solve_part_1()}")
+    print(f"Part 1: {solve_part_1("data/day8.txt", n=1000, k=3)}")
